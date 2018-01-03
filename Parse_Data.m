78a56a1e53d8d0006ebeda57cf82753a97f6a5fc
ykonda <yuria.konda@tuhh.de>
2018-01-03 13:34:14 +0100

    Get force data, find peaks, plot force in x and z direction. Get oct data, show image.

--- Parse_Data.m
+++ Parse_Data.m
@@ -1,28 +1,51 @@
-%% 
-folder_path = '/media/yuria/STIIIIICK';
+%% Path to data
+
+folder_path = '';
 %% FORCES
 
-file_forces = '/forces/p1m1_1.txt';
-force_path = strcat(folder_path, file_forces);
-force_data = dlmread(force_path);
-force_time = force_data(:,1);
-force_z = force_data(:, 4);
-force_x = force_data(:, 2);
+% Get data
+f_path = strcat(folder_path, 'forces/p1m1.txt');
+f_data = dlmread(f_path);
+f_time = f_data(:,1);
+f_z = f_data(:, 4);
+f_x = f_data(:, 2);
 
+% Remove offset
+f_x = f_x - mean(f_x);
+
+% Find value, location, and width of peaks
+[f_pks, f_locs, f_wds] = findpeaks(f_x,...
+                            'MinPeakHeight', 0.5,...
+                            'MinPeakDistance', 100);
+
+% Time real measurement starts
+f_start = f_locs(end) + f_wds(end)/2;
+figure;
+plot(f_data(f_start:end,2));
+                       
 % PLOTS
+figure;
+forceX_plot = subplot(3,1,1);
+plot(forceX_plot, f_time, f_x);
+title('Force Sensor Data');
+xlabel('Time');
+ylabel('Force X');
 
-% forceX_plot = subplot(2,1,1);
-% plot(forceX_plot, force_time, force_x);
-% forceZ_plot = subplot(2, 1, 2);
-% plot(forceZ_plot, force_time, force_z);
+forceZ_plot = subplot(3, 1, 2);
+plot(forceZ_plot, f_time, f_z);
+xlabel('Time');
+ylabel('Force Z');
+clear forceX_plot, forceZ_plot;
 
-clear file_forces, force_path, forceX_plot, forceZ_plot;
+clear file_forces, f_path;
 
 %% OCT
-file_oct = '/oct/p1m1.bin';
-file_timestamp = '/oct/p1m1.bin__timestamp.txt';
-oct_path = strcat(folder_path, file_oct);
-timestamp_path = strcat(folder_path, file_timestamp);
-oct_time = dlmread(timestamp_path);
-oct = fopen(oct_path);
-oct_data = fread(oct_path);
+
+o_path = strcat(folder_path, 'oct/p1m1.bin');
+o_time_path = strcat(folder_path, 'oct/p1m1_timestamp.txt');
+file_id = fopen(o_path);
+o_data = fread(file_id, [512, 108900], 'float');
+o_time = dlmread(o_time_path);
+
+o_plot = subplot(3, 1, 3);
+plot(o_plot, o_data);
