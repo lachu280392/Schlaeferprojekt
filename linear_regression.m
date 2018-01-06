9cac20d8fa59734599cc27d3da7278853d8c9ce6
felixwege <felix.wege@tuhh.de>
2018-01-06 20:12:57 +0100

    fix mistake in linear regression

--- linear_regression.m
+++ linear_regression.m
@@ -7,7 +7,6 @@ for p = 1:1
         force_path = strcat(preprocessed_data_path, 'forces/', file, '_forces.bin');
         force_file_id = fopen(force_path);
         force_data = fread(force_file_id, Inf, 'float');
-		force_data = force_data';
 
         oct_path = strcat(preprocessed_data_path, 'oct/', file, '_oct.bin');
         oct_file_id = fopen(oct_path);
@@ -17,7 +16,6 @@ for p = 1:1
 		linear_model = fitlm(oct_data, force_data);
 
 		force_prediction = predict(linear_model, oct_data);
-		force_error = force_data - force_prediction;
 	end
 end
 
