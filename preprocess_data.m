60fc7298f74decedfb5b60e7e37176a97e10d0df
felixwege <felix.wege@tuhh.de>
2018-01-06 17:18:26 +0100

    rename f and o to force and oct

--- preprocess_data.m
+++ preprocess_data.m
@@ -9,88 +9,88 @@ for p = 1:1
 
         %% Read data
         % Force
-        f_path = strcat(measured_data_path, 'forces/', file, '.txt');
-        f_data = dlmread(f_path);
-        f_time = f_data(:, 1);
-        f_data = f_data(:, 4);
+        force_path = strcat(measured_data_path, 'forces/', file, '.txt');
+        force_data = dlmread(force_path);
+        force_time = force_data(:, 1);
+        force_data = force_data(:, 4);
 
         % OCT
-        o_path = strcat(measured_data_path, 'oct/', file, '.bin');
-        o_time_path = strcat(measured_data_path, 'oct/', file, '_timestamp.txt');
-        o_file_id = fopen(o_path);
-        o_data = fread(o_file_id, [512, Inf], 'float');
-        o_time = dlmread(o_time_path);
+        oct_path = strcat(measured_data_path, 'oct/', file, '.bin');
+        oct_time_path = strcat(measured_data_path, 'oct/', file, '_timestamp.txt');
+        oct_file_id = fopen(oct_path);
+        oct_data = fread(oct_file_id, [512, Inf], 'float');
+        oct_time = dlmread(oct_time_path);
 
 		% timestamps for start and end of force measurement as well as start of oct measurement (end of oct measurement is calculated)
         time = readtable('data/timestamps.txt', 'Format', '%s%u%u%u');
         for i=1:9
             if strcmp(time.Var1(i), file)
-        	    f_start = time.Var2(i);
-				f_end = time.Var3(i);
-				f_number_of_samples = f_end - f_start + 1;
-				f_sampling_frequency = 10^6 * size(f_time, 1) / (f_time(end) - f_time(1));
-        	    o_start = time.Var4(i);
-				o_sampling_frequency = 100 * size(o_time, 1) / (o_time(end) - o_time(1));
-				o_number_of_samples = round(f_number_of_samples * o_sampling_frequency / f_sampling_frequency);
-				o_end = o_start + o_number_of_samples - 1;
+        	    force_start = time.Var2(i);
+				force_end = time.Var3(i);
+				force_number_of_samples = force_end - force_start + 1;
+				force_sampling_frequency = 10^6 * size(force_time, 1) / (force_time(end) - force_time(1));
+        	    oct_start = time.Var4(i);
+				oct_sampling_frequency = 100 * size(oct_time, 1) / (oct_time(end) - oct_time(1));
+				oct_number_of_samples = round(force_number_of_samples * oct_sampling_frequency / force_sampling_frequency);
+				oct_end = oct_start + oct_number_of_samples - 1;
             end
         end
 
         % Remove offset
-        f_data = f_data(f_start:f_end);
-        o_data = o_data(:, o_start:o_end);
+        force_data = force_data(force_start:force_end);
+        oct_data = oct_data(:, oct_start:oct_end);
 
 		% smooth and interpolate
-		f_data = smooth(f_data, 317);
-		f_data = interp1(1:double(f_number_of_samples), f_data', linspace(1, double(f_number_of_samples), double(o_number_of_samples)));
+		force_data = smooth(force_data, 317);
+		force_data = interp1(1:double(force_number_of_samples), force_data', linspace(1, double(force_number_of_samples), double(oct_number_of_samples)));
 
         %% Tidy up data
 
         % Maximum values
-        [o_pks, o_locs] = max(o_data);
-        o_locs_smooth = smooth(o_locs);
-        axis_max = o_locs(1) + 70;
-        axis_min = o_locs(1) - 70;
+        [oct_pks, oct_locs] = max(oct_data);
+        oct_locs_smooth = smooth(oct_locs);
+        axis_max = oct_locs(1) + 70;
+        axis_min = oct_locs(1) - 70;
 
         %% Plot
 
         % Flip 
-        o_data_flip = flipud(o_data);
-        [~, o_locs_flip] = max(o_data_flip);
-        axis_max_flip = o_locs_flip(1) + 70;
-        axis_min_flip = o_locs_flip(1) - 70;
+        oct_data_flip = flipud(oct_data);
+        [~, oct_locs_flip] = max(oct_data_flip);
+        axis_max_flip = oct_locs_flip(1) + 70;
+        axis_min_flip = oct_locs_flip(1) - 70;
 
         figure;
 
         subplot(2,1,1);
-        plot(f_data);
-		xlim([0, size(f_data, 2)]);
+        plot(force_data);
+		xlim([0, size(force_data, 2)]);
         xlabel('Time');
         ylabel('Force Z');
         title('Force Sensor');
 
         subplot(2,1,2);
-        image(o_data_flip);
+        image(oct_data_flip);
         hold on;
-        plot(smooth(o_locs_flip), '.r');
-        axis([0, size(o_data, 2), axis_min_flip, axis_max_flip]);
+        plot(smooth(oct_locs_flip), '.r');
+        axis([0, size(oct_data, 2), axis_min_flip, axis_max_flip]);
         xlabel('Time');
         ylabel('Depth');
         title('OCT');
 
         %% Write into file
         cd data/preprocessed_data/forces/;
-        f_file_id = fopen(strcat(file, '_forces.bin'), 'w');
-        fwrite(f_file_id, f_data, 'float');
-        fclose(f_file_id);
+        force_file_id = fopen(strcat(file, '_forces.bin'), 'w');
+        fwrite(force_file_id, force_data, 'float');
+        fclose(force_file_id);
 
         cd ../oct/;
-        o_file_id = fopen(strcat(file, '_oct.bin'), 'w');
-        fwrite(o_file_id, o_data, 'float');
-        fclose(o_file_id);
+        oct_file_id = fopen(strcat(file, '_oct.bin'), 'w');
+        fwrite(oct_file_id, oct_data, 'float');
+        fclose(oct_file_id);
 
 		cd ../time/;
-        time = linspace(0, (size(f_data, 2) - 1) / 1000, size(f_data, 2));
+        time = linspace(0, (size(force_data, 2) - 1) / 1000, size(force_data, 2));
         t_file_id = fopen(strcat(file, '_time.bin'), 'w');
         fwrite(t_file_id, time, 'float');
         fclose(t_file_id);
@@ -101,11 +101,11 @@ for p = 1:1
 end;
 
 %% Clear
-clear f_file_id f_end f_path f_start f_time;
-clear o_end o_file_id o_start o_time t_file_id;
+clear force_file_id force_end force_path force_start force_time;
+clear oct_end oct_file_id oct_start oct_time t_file_id;
 clear p i m;
 clear file
-clear o_pks_flip o_locs_flip axis_max_flip axis_min_flip o_locs o_pks;
-clear axis_max axis_min f_data_plot file_id measured_data_path o_data_flip o_max_plot o_locs_smooth;
-clear o_path o_time_path;
+clear oct_pks_flip oct_locs_flip axis_max_flip axis_min_flip oct_locs oct_pks;
+clear axis_max axis_min force_data_plot file_id measured_data_path oct_data_flip oct_max_plot oct_locs_smooth;
+clear oct_path oct_time_path;
 clear all;
