fb39bad0e40877349763d090cce5edb90de5f21c
felixwege <felix.wege@tuhh.de>
2018-01-07 18:14:03 +0100

    decimate, filter oct, add options to disale plots and writing to file

--- preprocess_data.m
+++ preprocess_data.m
@@ -1,9 +1,16 @@
-%% Path to data
+clear all;
+% set 'no_write' to true to prevent writing data to files
+no_write = false;
+% set 'no_plots' to true to prevent plots
+no_plots = true;
 
+decimation = 100;
+
+% Path to data
 measured_data_path = 'data/measured_data/';
 
-for p = 1:1
-    for m = 1:1
+for p = 1:3
+    for m = 1:3
         file = strcat('p', num2str(p), 'm', num2str(m));
         disp(file);
 
@@ -40,72 +47,65 @@ for p = 1:1
         force_data = force_data(force_start:force_end);
         oct_data = oct_data(:, oct_start:oct_end);
 
-		% smooth and interpolate
+		% filter, interpolate, sample
 		force_data = smooth(force_data, 317);
 		force_data = interp1(1:double(force_number_of_samples), force_data', linspace(1, double(force_number_of_samples), double(oct_number_of_samples)));
-
-        %% Tidy up data
-
-        % Maximum values
-        [oct_pks, oct_locs] = max(oct_data);
-        oct_locs_smooth = smooth(oct_locs);
-        axis_max = oct_locs(1) + 70;
-        axis_min = oct_locs(1) - 70;
+		force_data = force_data(1:decimation:end);
+		oct_data = oct_data(:, 1:decimation:end);
+		for i=1:size(oct_data, 1)
+			oct_data(i, :) = smooth(oct_data(i, :));
+		end
 
         %% Plot
+		if (not(no_plots))
+        	% Maximum values
+       		[oct_pks, oct_locs] = max(oct_data);
+        	oct_locs_smooth = smooth(oct_locs);
+        	% Flip 
+        	oct_data_flip = flipud(oct_data);
+        	[~, oct_locs_flip] = max(oct_data_flip);
+        	axis_max_flip = oct_locs_flip(1) + 70;
+        	axis_min_flip = oct_locs_flip(1) - 70;
 
-        % Flip 
-        oct_data_flip = flipud(oct_data);
-        [~, oct_locs_flip] = max(oct_data_flip);
-        axis_max_flip = oct_locs_flip(1) + 70;
-        axis_min_flip = oct_locs_flip(1) - 70;
+        	figure;
 
-        figure;
+        	subplot(2,1,1);
+        	plot(force_data);
+			xlim([0, size(force_data, 2)]);
+        	xlabel('Time');
+        	ylabel('Force Z');
+        	title('Force Sensor');
 
-        subplot(2,1,1);
-        plot(force_data);
-		xlim([0, size(force_data, 2)]);
-        xlabel('Time');
-        ylabel('Force Z');
-        title('Force Sensor');
-
-        subplot(2,1,2);
-        image(oct_data_flip);
-        hold on;
-        plot(smooth(oct_locs_flip), '.r');
-        axis([0, size(oct_data, 2), axis_min_flip, axis_max_flip]);
-        xlabel('Time');
-        ylabel('Depth');
-        title('OCT');
+        	subplot(2,1,2);
+        	image(oct_data_flip);
+        	hold on;
+        	plot(smooth(oct_locs_flip), '.r');
+        	axis([0, size(oct_data, 2), axis_min_flip, axis_max_flip]);
+        	xlabel('Time');
+        	ylabel('Depth');
+        	title('OCT');
+		end
 
         %% Write into file
-        cd data/preprocessed_data/forces/;
-        force_file_id = fopen(strcat(file, '_forces.bin'), 'w');
-        fwrite(force_file_id, force_data, 'float');
-        fclose(force_file_id);
+		if (not(no_write))
+        	cd data/preprocessed_data/forces/;
+        	force_file_id = fopen(strcat(file, '_forces.bin'), 'w');
+        	fwrite(force_file_id, force_data, 'float');
+        	fclose(force_file_id);
 
-        cd ../oct/;
-        oct_file_id = fopen(strcat(file, '_oct.bin'), 'w');
-        fwrite(oct_file_id, oct_data, 'float');
-        fclose(oct_file_id);
+        	cd ../oct/;
+        	oct_file_id = fopen(strcat(file, '_oct.bin'), 'w');
+        	fwrite(oct_file_id, oct_data, 'float');
+        	fclose(oct_file_id);
 
-		cd ../time/;
-        time = linspace(0, (size(force_data, 2) - 1) / 1000, size(force_data, 2));
-        t_file_id = fopen(strcat(file, '_time.bin'), 'w');
-        fwrite(t_file_id, time, 'float');
-        fclose(t_file_id);
-        cd ..;
-		cd ..;
-		cd ..;
+			cd ../time/;
+        	time = linspace(0, (size(force_data, 2) - 1) / 1000, size(force_data, 2));
+        	t_file_id = fopen(strcat(file, '_time.bin'), 'w');
+        	fwrite(t_file_id, time, 'float');
+        	fclose(t_file_id);
+        	cd ..;
+			cd ..;
+			cd ..;
+		end
     end;
 end;
-
-%% Clear
-clear force_file_id force_end force_path force_start force_time;
-clear oct_end oct_file_id oct_start oct_time t_file_id;
-clear p i m;
-clear file
-clear oct_pks_flip oct_locs_flip axis_max_flip axis_min_flip oct_locs oct_pks;
-clear axis_max axis_min force_data_plot file_id measured_data_path oct_data_flip oct_max_plot oct_locs_smooth;
-clear oct_path oct_time_path;
-clear all;
