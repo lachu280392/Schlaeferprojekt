ece88b4665b07a60d00aca951dde6421ed04e7dc
felixwege <felix.wege@tuhh.de>
2018-01-19 15:49:16 +0100

    use features extraction in rnn

--- rnn.m
+++ rnn.m
@@ -10,65 +10,32 @@ metal_files = convertCharsToStrings(metal_files);
 metal_files = strsplit(metal_files);
 metal_files = erase(metal_files, '.txt');
 
-mean_squared_error = [];
-
-% perform leave-one-out cross validation with all metal files
+force_data = [];
+features = [];
 for file_index = 1:1%numel(metal_files)
-    % decide which files are used for training and validation
-    validation_file = metal_files(file_index)
-    training_files = metal_files(metal_files~=validation_file);
-
-    % force_data for validation
-    force_path = strcat(metal_path, 'forces/', validation_file);
+    disp(metal_files(file_index));
+    % concatenate force data of all metal files
+    force_path = strcat(metal_path, 'forces/', metal_files(file_index));
     force_file_id = fopen(force_path);
-    force_validation = fread(force_file_id, Inf, 'float');
+    force_buffer = fread(force_file_id, Inf, 'float');
+    force_data = cat(1, force_data, force_buffer);
 
-    % oct_data for validation
-    oct_path = strcat(metal_path, 'oct/', validation_file);
+    % concatenate the features extracted from all metal files
+    oct_path = strcat(metal_path, 'oct/', metal_files(file_index));
     oct_file_id = fopen(oct_path);
-    oct_validation = fread(oct_file_id, [depth, Inf], 'float')';
+    oct_data  = fread(oct_file_id, [depth, Inf], 'float');
+    features = cat(1, features, extract_features(oct_data));
+end
 
-    force_training = [];
-    oct_training = [];
-    for training_files_index = 1:numel(training_files)
-        force_path = strcat(metal_path, 'forces/', training_files(training_files_index));
-        force_file_id = fopen(force_path);
-        force_buffer = fread(force_file_id, Inf, 'float');
-        force_training = cat(1, force_training, force_buffer);
+% convert to cell arrays
+depth_at_maximum_intensity = num2cell(features.depth_at_maximum_intensity);
+force_data = num2cell(force_data');
 
-        oct_path = strcat(metal_path, 'oct/', training_files(training_files_index));
-        oct_file_id = fopen(oct_path);
-        oct_buffer = fread(oct_file_id, [depth, Inf], 'float');
-        oct_training = cat(1, oct_training, oct_buffer');
-    end
+% create and train recurrent neural network
+net = layrecnet(1, 5);
+net.trainParam.showCommandLine = true;
+net.trainFcn = 'trainlm';
+[net, training_record] = train(net, depth_at_maximum_intensity, force_data);
 
-    % extract index (depth) at maximum intensity
-    [~, maximum_intensity_index_training] = max(oct_training, [], 2);
-    [~, maximum_intensity_index_validation] = max(oct_validation, [], 2);
-
-    % create and train recurrent neural network
-    maximum_intensity_index_training = num2cell(maximum_intensity_index_training');
-    force_training = num2cell(force_training');
-    net = layrecnet(1, 5);
-    net.trainParam.showCommandLine = true;
-    net.trainFcn = 'trainbr';
-    [net, training_record] = train(net, maximum_intensity_index_training, force_training);
-
-    % predict force
-    maximum_intensity_index_validation = num2cell(maximum_intensity_index_validation');
-    force_prediction = net(maximum_intensity_index_validation);
-    force_prediction = cell2mat(force_prediction);
-
-    % model evaluation (see https://en.wikipedia.org/wiki/Regression_validation)
-    mean_squared_error = [mean_squared_error, 0];
-
-    % plots
-    figure;
-    hold on;
-    plot(force_validation);
-    plot(force_prediction);
-    xlim([0, size(force_validation, 1)]);
-    title(validation_file);
-
-    % save model
-    model_path = 'models/rnn'; save(model_path, 'net'); end
+% save model
+model_path = 'models/rnn'; save(model_path, 'net');
