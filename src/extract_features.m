f010a6617b00063aa8813289d0ebcee02fdfc9d5
felixwege <felix.wege@tuhh.de>
2018-01-20 20:02:41 +0100

    improve feature extraction

--- src/extract_features.m
+++ src/extract_features.m
@@ -7,17 +7,16 @@ function features = extract_features(oct_data)
     % the reference depth is used to try to remove the zero line
     reference_intensity = oct_sorted(1, 1);
     reference_depth = find(oct_data(:, 1) == reference_intensity);
-    reference_depth = reference_depth - round(0.05 * size(oct_data, 1));
 
     maxima_to_consider = 42;
-    depth_at_maximum_intensity = [];
+    depth_at_maximum_intensity = [reference_depth];
     for i = 1:size(oct_data, 2)
         % threshold for candidates of maximal intensity
         intensity_threshold = 0.85 * oct_sorted(1, i);
 
         % find intensities that are larger than the threshold
         j = 2;
-        maximum_intensities = [];
+        maximum_intensities = [reference_depth];
         while ((oct_sorted(j, i) > intensity_threshold) & j < maxima_to_consider)
             maximum_intensities = cat(1, maximum_intensities, oct_sorted(j, i));
             j = j + 1;
@@ -26,14 +25,23 @@ function features = extract_features(oct_data)
         % initialization with the reference depth makes sure that no depth larger than it is considered
         depth_buffer = [reference_depth];
 
+
         % find the depths of all maximum intensities
         for k = 1:numel(maximum_intensities)
             depth_buffer = cat(1, depth_buffer, find(oct_data(:, i) == maximum_intensities(k)));
         end
 
-        % use only the lowest depth
-        depth_at_maximum_intensity = cat(1, depth_at_maximum_intensity, min(depth_buffer));
+        % check if the dew depth candidate is in the vicinity of the previous depth
+        new_depth = min(depth_buffer);
+        previous_depth = depth_at_maximum_intensity(i);
+        if (abs(new_depth - previous_depth) < 10)
+            depth_at_maximum_intensity = cat(1, depth_at_maximum_intensity, new_depth);
+        else
+            depth_at_maximum_intensity = cat(1, depth_at_maximum_intensity, previous_depth);
+        end
     end
+
+    depth_at_maximum_intensity = depth_at_maximum_intensity(2:end);
 
     % subtract reference depth
     depth_at_maximum_intensity = depth_at_maximum_intensity - reference_depth;
