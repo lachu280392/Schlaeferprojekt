6996c1f1ddf2ef736162b243c3ed0f46b5270c96
felixwege <felix.wege@tuhh.de>
2018-01-21 15:44:52 +0100

    verify_feature_extraction

---
+++ src/verify_feature_extraction.m
@@ -0,0 +1,24 @@
+depth = 2 * 50 + 1;
+
+metal_path  = '../preprocessed_data/metal/';
+metal_files = ls(strcat(metal_path, 'forces/'));
+metal_files = metal_files(1:end - 1);
+metal_files = convertCharsToStrings(metal_files);
+metal_files = strsplit(metal_files);
+
+for file_index = 1:numel(metal_files)
+    file = metal_files(file_index);
+
+    oct_path = strcat(metal_path, 'oct/', training_set_files(training_set_index));
+    oct_file_id = fopen(oct_path);
+    oct_data = fread(oct_file_id, [depth, inf], 'float');
+    features_buffer = extract_features(oct_data);
+    depth_at_maximum_intensity = features_buffer.depth_at_maximum_intensity;
+
+    figure;
+    hold on;
+    image(oct_data);
+    plot(depth_at_maximum_intensity, '.r');
+    title(file);
+end
+
