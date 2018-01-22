5325e17c6d4e7448869dd9a6c5b1d2f06cb04f77
felixwege <felix.wege@tuhh.de>
2018-01-22 13:39:21 +0100

    separate test script for rnn

--- src/rnn.m
+++ src/rnn.m
@@ -10,32 +10,36 @@ metal_files = convertCharsToStrings(metal_files);
 metal_files = strsplit(metal_files);
 metal_files = erase(metal_files, '.txt');
 
-force_data = [];
-features = [];
-for file_index = 1:1%numel(metal_files)
+force_training = [];
+depth_at_maximum_intensity_training = [];
+for file_index = 1:numel(metal_files)
     disp(metal_files(file_index));
     % concatenate force data of all metal files
     force_path = strcat(metal_path, 'forces/', metal_files(file_index));
     force_file_id = fopen(force_path);
     force_buffer = fread(force_file_id, Inf, 'float');
-    force_data = cat(1, force_data, force_buffer);
+    force_buffer = force_buffer - mean(force_buffer(1:9));
+    force_training = cat(1, force_training, force_buffer);
 
     % concatenate the features extracted from all metal files
     oct_path = strcat(metal_path, 'oct/', metal_files(file_index));
     oct_file_id = fopen(oct_path);
     oct_buffer  = fread(oct_file_id, [depth, Inf], 'float');
-    features = cat(1, features, extract_features(oct_buffer));
+    features_buffer = extract_features(oct_buffer);
+    depth_buffer = (features_buffer.depth_at_maximum_intensity);
+    depth_buffer = depth_buffer - mean(depth_buffer(1:9));
+    depth_at_maximum_intensity_training = cat(2, depth_at_maximum_intensity_training, depth_buffer);
 end
 
 % convert to cell arrays
-depth_at_maximum_intensity = num2cell(features.depth_at_maximum_intensity);
-force_data = num2cell(force_data');
+depth_at_maximum_intensity_training = num2cell(depth_at_maximum_intensity_training);
+force_training = num2cell(force_training');
 
 % create and train recurrent neural network
 net = layrecnet(1, 5);
 net.trainParam.showCommandLine = true;
 net.trainFcn = 'trainlm';
-[net, training_record] = train(net, depth_at_maximum_intensity, force_data);
+[net, training_record] = train(net, depth_at_maximum_intensity_training, force_training);
 
 % save model
 model_path = '../models/rnn';
---
+++ src/test_rnn.m
@@ -0,0 +1,50 @@
+clear all;
+
+% load net
+net_path = '../models/rnn';
+net = load(net_path);
+net = struct2cell(net);
+net = net{1};
+
+% all phantom files, i. e. the testing set
+phantoms_path = '../preprocessed_data/phantoms/';
+phantoms_files = ls(strcat(phantoms_path, 'forces/'));
+phantoms_files = phantoms_files(1:end - 1);
+phantoms_files = convertCharsToStrings(phantoms_files);
+phantoms_files = strsplit(phantoms_files);
+phantoms_files = erase(phantoms_files, '.txt');
+
+depth = 2 * 50 + 1;
+
+mean_squared_error = [];
+for file_index = 1:numel(phantoms_files)
+    testing_file = phantoms_files(file_index);
+
+    % force_data for testing
+    force_path = strcat(phantoms_path, 'forces/', testing_file);
+    force_file_id = fopen(force_path);
+    force_testing = fread(force_file_id, Inf, 'float');
+    force_testing = force_testing - mean(force_testing(1:9));
+
+    % oct_data for testing
+    oct_path = strcat(phantoms_path, 'oct/', testing_file);
+    oct_file_id = fopen(oct_path);
+    oct_buffer = fread(oct_file_id, [depth, Inf], 'float');
+    features_buffer = extract_features(oct_buffer);
+    depth_at_maximum_intensity_testing = (features_buffer.depth_at_maximum_intensity);
+    depth_at_maximum_intensity_testing = depth_at_maximum_intensity_testing - mean(depth_at_maximum_intensity_testing(1:9));
+    depth_at_maximum_intensity_testing = num2cell(depth_at_maximum_intensity_testing);
+
+    % model evaluation (see https://en.wikipedia.org/wiki/Regression_validation)
+    force_prediction = net(depth_at_maximum_intensity_testing);
+    force_prediction = cell2mat(force_prediction)';
+    mean_squared_error = [mean_squared_error, immse(force_prediction, force_testing)];
+
+    % plots
+    figure;
+    hold on;
+    plot(force_testing);
+    plot(force_prediction);
+    xlim([0, size(force_testing, 1)]);
+    title(testing_file);
+end
