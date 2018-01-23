fd7cbd5a989b982ed42830ee6cf61380e7ead3c5
felixwege <felix.wege@tuhh.de>
2018-01-23 03:02:34 +0100

    better parameters

--- src/extract_features.m
+++ src/extract_features.m
@@ -1,6 +1,11 @@
 function features = extract_features(oct_data)
     assert(size(oct_data, 1) < size(oct_data, 2), 'The time axis of the OCT data should be the second dimension. Please consider transposing your data!');
 
+    %[~, depth_at_maximum_intensity] = max(oct_data);
+    %oct_5th_percentile = prctile(depth_at_maximum_intensity, 5);
+    %oct_95th_percentile = prctile(depth_at_maximum_intensity, 95);
+    %oct_data = oct_data(oct_data > oct_5th_percentile - 5 & oct_data < oct_95th_percentile + 5);
+
     % sort oct data to easily find maxima
     oct_sorted = sort(oct_data, 1, 'descend');
 
@@ -12,7 +17,7 @@ function features = extract_features(oct_data)
     depth_at_maximum_intensity = [reference_depth];
     for i = 1:size(oct_data, 2)
         % threshold for candidates of maximal intensity
-        intensity_threshold = 0.77 * oct_sorted(1, i);
+        intensity_threshold = 0.75 * oct_sorted(1, i);
 
         % find intensities that are larger than the threshold
         j = 2;
@@ -33,7 +38,7 @@ function features = extract_features(oct_data)
         % check if the dew depth candidate is in the vicinity of the previous depth
         new_depth = min(depth_buffer);
         previous_depth = depth_at_maximum_intensity(i);
-        if (abs(new_depth - previous_depth) < 10)
+        if (abs(new_depth - previous_depth) < 15)
             depth_at_maximum_intensity = cat(1, depth_at_maximum_intensity, new_depth);
         else
             depth_at_maximum_intensity = cat(1, depth_at_maximum_intensity, previous_depth);
@@ -46,7 +51,7 @@ function features = extract_features(oct_data)
     depth_at_maximum_intensity = filloutliers(depth_at_maximum_intensity, 'linear', 'movmean', 64);
 
     % moving average for smoothing
-    depth_at_maximum_intensity = smooth(depth_at_maximum_intensity, 32);
+    depth_at_maximum_intensity = smooth(depth_at_maximum_intensity, 64);
 
     % create table of features
     features = table(depth_at_maximum_intensity');
