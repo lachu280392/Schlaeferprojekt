c5698cfd918847063006378db7e1d9e1e711cc7e
felixwege <felix.wege@tuhh.de>
2018-01-23 18:02:59 +0100

    fix wrong normalization

--- src/linear_regression.m
+++ src/linear_regression.m
@@ -78,7 +78,7 @@ for cross_validation_iteration = 1:number_of_cross_validations
         oct_buffer = fread(oct_file_id, [depth, Inf], 'float');
         features_buffer = extract_features(oct_buffer);
         depth_at_maximum_intensity_validation = (features_buffer.depth_at_maximum_intensity)';
-        depth_at_maximum_intensity_validation = depth_at_maximum_intensity_validation - mean(depth_buffer(1:9));
+        depth_at_maximum_intensity_validation = depth_at_maximum_intensity_validation - mean(depth_at_maximum_intensity_validation(1:9));
 
         % predict force
         force_prediction = predict(linear_model, depth_at_maximum_intensity_validation);
